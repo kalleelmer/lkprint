08ccd77a70bd70fd6bed05c31bab3f21cb3e2485
Kalle Elmér <kalle@silvertejp.nu>
2017-12-30 22:44:57 +0100

    Lägg till byggnummer i paketversion.

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
-rm -rf target
-mkdir target
-cp -r deb target/package
-rsync -r src target/package/opt/lkprint --exclude "__pycache__"
-cd target
-dpkg-deb --build package .
+
+if [ $# -eq 0 ]
+then
+	echo "Usage: build.sh <build number>"
+else
+	rm -rf target
+	mkdir target
+	cp -r deb target/package
+	rsync -r src target/package/opt/lkprint --exclude "__pycache__"
+	cd target
+	sed -i "s/BUILDNUMBER/$1/" package/DEBIAN/control
+	dpkg-deb --build package .
+fi
 
